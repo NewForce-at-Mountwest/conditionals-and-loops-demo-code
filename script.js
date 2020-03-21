283eb4ebee0717dc0786a5bf3a2960290c573f4b
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-03-21 11:18:52 -0400

    demo code for conditionals

---
+++ script.js
@@ -0,0 +1,142 @@
+
+// if (2 + 2 === 5) {
+//   console.log("Math works!");
+// } else {
+//   console.log("Math is broken.");
+// }
+
+
+// var likesPizza = false;
+
+// console.log(likesPizza)
+
+// if(likesPizza === true){
+//   console.log("Have some pizza!")
+// }
+
+// var favoriteFood = "Pizza";
+
+// if(favoriteFood === "tacos"){
+//   console.log("Heck yeah! You have great taste!")
+// } else if (favoriteFood === "pizza") {
+//   console.log("You have pretty good taste but have you tried tacos?")
+// } else {
+//   console.log("You have terrible taste. Please at a taco.")
+// }
+
+// // You can have multiple conditions!
+//  var time ="afternoon";
+
+// if (time === "morning"){
+//   console.log("Good morning!");
+// } else if (time === "afternoon"){
+//   console.log("Good afternoon!");
+// } else if (time === "evening"){
+//   console.log("Good evening!");
+// } else {
+//   console.log("Hello!");
+// }
+
+
+// // -- Lightning exercise! -- //
+// // In your JavaScript file, declare a new variable called likesToTravel and set it equal to true or false.
+
+// var likesToTravel = true;
+
+// // If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.
+
+// if(likesToTravel === true){
+//   console.log("Bon voyage except not really bc coronavirus")
+// } else {
+//   console.log("Good, because we're quarantined anyway")
+// }
+
+// Triple equals will check to see if the type and the value are equal
+// Double equals won't
+
+// var age = 21
+// if(age >= 21){
+//   console.log("You can drink!")
+// } else if(age >= 18) {
+//   console.log("You can vote!")
+// } else {
+//   console.log("You can't vote OR drink")
+// }
+
+// if(age !== 21){
+//   console.log("You are definitely not 21")
+// }
+
+var isWereWolf = false;
+var isFullMoon = true;
+var isHungry = false;
+
+if(isWereWolf === true && isFullMoon == true){
+  console.log("Run for your life! I'm gonna turn into a wolf!")
+} else if (isWereWolf === true && isFullMoon === false){
+  console.log("I'm still a werewolf but we're safe")
+} else if(isWereWolf === false && isFullMoon === true){
+  console.log("It's a beautiful full moon, and thankfully I am not a werewolf")
+}
+
+if(isWereWolf === true || isHungry === true){
+  console.log("Time for a snack")
+}
+
+
+
+// Fried Pickles are the Best
+// In your JavaScript file, declare a new variable called favoriteFood and set it equal to your favorite food.
+var favoriteFood = "tacos";
+// Write a conditional that checks to see if the favoriteFood variable is equal to "fried pickles" OR "pepperoni rolls". If so, log "Congratulations! You have excellent taste!" to the console. Otherwise, log "Clearly you have not tried fried pickles or pepperoni rolls." to the console.
+
+if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls"){
+  console.log("You have great taste!")
+} else {
+  console.log("Have you tried fried pickles or pepperoni rolls?")
+}
+
+
+
+
+
+
+
+// >, <, <=, >=
+
+// !==
+
+// Logical operators -- and and or
+
+// --- Lightning Exercise! -- //
+
+
+
+
+
+
+
+
+// -- LOOPS -- //
+
+// var i = 1;
+
+// while(i < 20){
+//   console.log("Hello, world");
+//   i++;
+// }
+
+// var moviesArray = ["Casablanca", "Star Wars", "Singing in the Rain", "The Wizard of Oz", "Die Hard"];
+
+// var i = 0;
+
+// while(i < moviesArray.length){
+//   console.log(moviesArray[i]);
+//   i++;
+// }
+
+// ----- Lightning Exercise! ----- //
+
+// Rewrite the same movie array loop as a for loop
+
+// ------ Lightning Exercise again! ----//
